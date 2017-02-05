4e75c70903a6b15be33147cba1fef9e20dcf500e
vlad <vladYamp@mail.ru>
2017-02-05 15:09:18 +0400

    recive from sanya

--- app/models/anime.js
+++ app/models/anime.js
@@ -4,6 +4,7 @@ export default DS.Model.extend({
 	cover: DS.attr('file'),
 	title: DS.attr('string'),
 	manufacturer: DS.attr('string'),
+	video_preview: DS.attr('string'),
 	genres: DS.hasMany('genre'),
 	type: DS.attr('string'),
 	date: DS.attr('string'),
